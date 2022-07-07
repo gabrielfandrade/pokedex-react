d89be1c3c8294781213905f83477a01aed59b77d
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-07-07 12:51:51 -0300

    React Hooks Loading

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,15 +1,15 @@
-import { Component } from 'react';
+import React from 'react';
 import '../components/Loading.css';
 
-export default class Loading extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="pokeball" />          
-        <div className="loading-message">
-          Loading...
-        </div>
+function Loading() {
+  return (
+    <div className="wrapper">
+      <div className="pokeball" />          
+      <div className="loading-message">
+        Loading...
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Loading;
